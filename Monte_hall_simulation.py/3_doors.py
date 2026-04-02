54c3ff793eadd00032e6c162f05df05af91908c6
Acash-bits <akashandwork@gmail.com>
2026-04-02 12:47:53 +0530

    Implement Monte Hall simulation logic with user input for door selection

---
+++ Monte_hall_simulation.py/3_doors.py
@@ -0,0 +1,26 @@
+import random
+# import matplotlib.pylot as plt
+
+# Placing the car inside the list at a random number
+for i in range(1):
+    # Doors for the simulation
+    behind_gate = ['goat', 'goat', 'goat']
+    
+    # Logic to put car at a random spot in list
+    car = random.randint(0,2)
+    behind_gate[car] = 'car'
+
+# Taking the input from the user and opening the door with the gate
+choice = input("Choose the door from 1-3: ")
+print(f"You have chosen the gate number: {choice}")
+
+# Opening the door which has goat behind it
+print(f"\nThere is a goat behind the door {behind_gate.index('goat')+1}")
+second_choice = input('\nWould you like the switch the door, if yes please enter ' \
+'the new door number if not enter the earlier door number: ')
+second_input = int(second_choice) - 1
+
+if behind_gate[second_input] == 'car':
+    print('You won the car')
+else:
+    print(f"The car was behind door {behind_gate.index('car')}")
